db098fd910bec4d5e03cabb47c90b965cac18d35
Harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2018-10-12 07:56:11 -0400

    Lab 3: Updated Ex3

--- Lab3/HarpreetKaur_COMP254_Lab3_Ex3/src/TraceFile.java
+++ Lab3/HarpreetKaur_COMP254_Lab3_Ex3/src/TraceFile.java
@@ -1,9 +1,32 @@
+import java.io.File;
 
 public class TraceFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String fileName = "test.txt";
+		File rootPath = new File("E:\\Professional\\Projects\\COMP254_DataStructure");
+		String path = trace(rootPath, fileName);
+		if(!path.equals("")) {
+			System.out.println("File '" + fileName + "\' found at following paths: \n" + path);
+		}else {
+			System.out.println("File '" + fileName + "\'not found!");
+		}
+		
+	}
+	
+	public static String trace(File path, String fileName) {
+		String filePath = "";
+		File[] list = path.listFiles();
+		if(list!= null) {
+			for(File file : list) {
+				if(file.isDirectory()) {
+					filePath += trace(file, fileName);
+				}else if(file.getName().equalsIgnoreCase(fileName)) {
+					filePath += file.getParentFile() + "\\" + fileName + "\n";
+				}
+			}
+		}
+		return filePath;
 	}
 
 }
